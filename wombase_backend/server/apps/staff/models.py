e0cba3299cab5e0a45e77f4e2a03420a7b8b593a
Andriy Smyk <105750583+smykandriy@users.noreply.github.com>
2023-04-12 18:37:42 +0300

    added id as primary key

--- wombase_backend/server/apps/staff/models.py
+++ wombase_backend/server/apps/staff/models.py
@@ -50,7 +50,7 @@ class EmployeeManager(BaseUserManager):
 
 
 class Employee(AbstractBaseUser, PermissionsMixin):
-    phone_number = models.CharField(max_length=15, unique=True, primary_key=True)
+    phone_number = models.CharField(max_length=15, unique=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField(blank=True, null=True, unique=True)
