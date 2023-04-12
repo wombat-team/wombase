b18da8f85b996db583b67db7a339fab4adbfd137
Andriy Smyk <105750583+smykandriy@users.noreply.github.com>
2023-04-12 18:38:11 +0300

    added serializer for roles view

--- wombase_backend/server/apps/staff/serializers.py
+++ wombase_backend/server/apps/staff/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
-from .models import Employee
+from .models import Employee, EmployeeRole
 
 
-class EmployeeSerializer(serializers.ModelSerializer):
+class EmployeeDetailsSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=32, write_only=True)
+
     class Meta:
         model = Employee
-        fields = ('phone_number', 'first_name', 'last_name', 'email', 'role')
+        fields = ('phone_number', 'first_name', 'last_name', 'password', 'email', 'role')
+
+
+class EmployeeListCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=32, write_only=True)
+
+    class Meta:
+        model = Employee
+        fields = ('id', 'phone_number', 'first_name', 'last_name', 'password', 'email', 'role')
+
+
+class EmployeeRoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmployeeRole
+        fields = ('name',)
