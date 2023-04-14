3465f7cea65265934696417d8f89cca89d584ba4
Marko Yaminsky <marko.yaminskiy03@gmail.com>
2023-04-14 18:31:20 +0300

    added tests for tools app

--- wombase_backend/server/apps/core/tests.py
+++ wombase_backend/server/apps/core/tests.py
@@ -1,8 +1,13 @@
+import random
+import string
+
 from django.db.models import Model
 from django.test import TestCase
+from rest_framework.test import APIClient
 
 
 class AbstractTestMixin(TestCase):
+    client_class = APIClient
     serializer = None
     url: str = None
 
@@ -58,22 +63,50 @@ class AbstractTestMixin(TestCase):
 
 
 class AbstractListCreateViewTest(AbstractTestMixin):
-    creation_data = {}
+    create_data = {}
 
-    def list_test_method(self):
+    def get_create_data(self):
+        return self.create_data
+
+    def get_valid_query_params(self):
+        return [field.name for field in self.model._meta.fields if not field.is_relation]
+
+    def get_queryset_contains_test(self):
+        query_parameters = self.get_valid_query_params()
+        query_param_random_value = random.choice(string.ascii_letters + string.digits)
+        for parameter in query_parameters:
+            response_data = self.client.get(
+                url := f"{self.url}?{parameter}={query_param_random_value}"
+            ).data
+            print(url, response_data)
+
+            expected_data_length = self.model.objects.filter(
+                **{f"{parameter}__icontains": query_param_random_value}
+            ).count()
+            self.assertEquals(
+                expected_data_length,
+                response_data_length := len(response_data),
+                f"Resulting queryset should be {expected_data_length} objects long. "
+                f"Actual length: {response_data_length}",
+            )
+            for instance in response_data:
+                self.assertTrue(
+                    query_param_random_value.lower()
+                    in (instance_parameter := instance.get(parameter).lower()),
+                    f"Query parameter fail. {query_param_random_value} is not in {instance_parameter}",
+                )
+
+    def basic_list_functionality_test(self):
         """Response list must contain all objects that were created and return 200 status code"""
         response = self.client.get(self.url)
-
         self.assertEqual(
-            status_code := response.status_code,
             200,
+            status_code := response.status_code,
             f"Status code should be 200, got {status_code}",
         )
-
         exptected_objects = [
             self.to_narrowed_dict(instance) for instance in self.model.objects.all()
         ]
-
         actual_objects = [dict(instance) for instance in response.data]
         self.assertListEqual(
             exptected_objects,
@@ -81,25 +114,29 @@ class AbstractListCreateViewTest(AbstractTestMixin):
             "Objects of present db dataset must be equal to objects returned by get request",
         )
 
-    def create_test_method(self):
+    def basic_create_functionality_test(self):
         """Post request must create new object in DB when data is valid, return 201 status code and created object"""
-        response = self.client.post(self.url, data=self.creation_data)
+        db_size_before_post = self.model.objects.count()
+        create_data = self.get_create_data()
+        response = self.client.post(self.url, data=create_data)
         self.assertEquals(
             201,
             response_code := response.status_code,
             f"Expected status code 201, got {response_code}",
         )
         response_data = response.data
-        for field in self.creation_data:
+        for field in create_data:
             if field == "password":
                 continue
             self.assertEquals(
-                exptected := self.creation_data.get(field),
+                exptected := create_data.get(field),
                 actual := response_data.get(field),
                 f"Field {field}: expected {exptected}, actual {actual}",
             )
         self.assertEquals(
-            3, self.model.objects.count(), "Newly created object is not in the DB"
+            db_size_before_post + 1,
+            self.model.objects.count(),
+            "Newly created object is not in the DB",
         )
 
 
@@ -109,6 +146,9 @@ class AbstractRetrieveUpdateDestroyViewTest(AbstractTestMixin):
     @property
     def available_object_pk(self) -> int:
         return self.model.objects.last().pk
+
+    def get_update_data(self):
+        return self.update_data
 
     def get_url(self) -> str:
         return super(self.__class__, self).url + str(self.available_object_pk)
@@ -121,30 +161,36 @@ class AbstractRetrieveUpdateDestroyViewTest(AbstractTestMixin):
             f"Expected status code 404, got {status_code}",
         )
 
-    def retrieve_test_method(self):
+    def basic_retrieve_functionality_test(self):
         """Get request must return object from db and status code 200"""
         response = self.client.get(self.get_url())
-        expected_object = self.to_narrowed_dict(
-            self.model.objects.get(pk=self.available_object_pk)
-        )
-        actual_object = self.to_narrowed_dict(response.data)
         self.assertEqual(
             200,
             actual_status_code := response.status_code,
             f"Reponse status code must be 200, got {actual_status_code}",
         )
+        expected_object = self.to_narrowed_dict(
+            self.model.objects.get(pk=self.available_object_pk)
+        )
+        actual_object = self.to_narrowed_dict(response.data)
         self.assertEqual(
             expected_object,
             actual_object,
             f"Expected {expected_object}, got {actual_object}",
         )
 
-    def update_test_method(self):
+    def basic_update_functionality_test(self):
         """Put request must return changed object data and changed password must be hashed. Status code 200"""
-        response = self.client.put(self.get_url(), data=self.update_data)
+        update_data = self.get_update_data()
+        response = self.client.put(self.get_url(), data=update_data)
         object_in_db = self.model.objects.get(pk=self.available_object_pk)
         self.assertEquals(
-            expected := self.to_narrowed_dict(self.update_data, include_blank=False),
+            200,
+            actual_status_code := response.status_code,
+            f"Expected status code 200, got {actual_status_code}",
+        )
+        self.assertEquals(
+            expected := self.to_narrowed_dict(update_data, include_blank=False),
             actual := self.to_narrowed_dict(object_in_db, include_blank=False),
             f"Object in db is not the same as in put request. Expected {expected}, got {actual}",
         )
@@ -153,23 +199,18 @@ class AbstractRetrieveUpdateDestroyViewTest(AbstractTestMixin):
             actual := self.to_narrowed_dict(object_in_db),
             f"Response data must match the data in db. Expected {expected}, got {actual}",
         )
-        self.assertEquals(
-            200,
-            actual_status_code := response.status_code,
-            f"Expecrted status code 200, got {actual_status_code}",
-        )
 
-    def delete_test_method(self):
+    def basic_delete_functionality_test(self):
         """Object must dissapear from db after deletion and status code must be 204"""
         url = self.get_url()
         deleted_object_pk = self.available_object_pk
         response = self.client.delete(url)
-        self.assertIsNone(
-            self.model.objects.filter(pk=deleted_object_pk).first(),
-            "Object is still present in the db. It must be deleted",
-        )
         self.assertEquals(
             204,
             actual_status_code := response.status_code,
             f"Expected status code 204, got {actual_status_code}",
+        )
+        self.assertIsNone(
+            self.model.objects.filter(pk=deleted_object_pk).first(),
+            "Object is still present in the db. It must be deleted",
         )
--- wombase_backend/server/apps/inventory/tests.py
+++ wombase_backend/server/apps/inventory/tests.py
@@ -1,3 +1,124 @@
-from django.test import TestCase
+from server.apps.core.tests import (
+    AbstractTestMixin,
+    AbstractListCreateViewTest,
+    AbstractRetrieveUpdateDestroyViewTest,
+)
+from server.apps.inventory.models import ToolCategory, Tool
+from server.apps.inventory.serializers import ToolCategorySerializer, ToolSerializer
+from server.apps.staff.models import Employee
 
-# Create your tests here.
+
+class ToolCategoryTestMixin(AbstractTestMixin):
+    url = "/tools/category/"
+
+    def setUp(self):
+        ToolCategory.objects.all().delete()
+        for category_name in ("Drill", "Screwdriver"):
+            ToolCategory.objects.create(
+                name=category_name, description=category_name * 2
+            )
+
+
+class TestToolCategoryListCreateAPIView(
+    ToolCategoryTestMixin, AbstractListCreateViewTest
+):
+    serializer = ToolCategorySerializer
+    create_data = {"name": "Saw", "description": "Saws"}
+
+    def test_list_tool_category(self):
+        self.basic_list_functionality_test()
+
+    def test_create_tool_category(self):
+        self.basic_create_functionality_test()
+
+
+class TestToolCategoryRetrieveUpdateDestroyAPIView(
+    ToolCategoryTestMixin, AbstractRetrieveUpdateDestroyViewTest
+):
+    serializer = ToolCategorySerializer
+    update_data = {"name": "Rasp", "description": "Smoothes"}
+
+    def test_request_by_unexisting_id(self):
+        self.request_by_unexisting_id()
+
+    def test_retrieve_tool_category(self):
+        self.basic_retrieve_functionality_test()
+
+    def test_update_tool_category(self):
+        self.basic_update_functionality_test()
+
+    def test_delete_tool_category(self):
+        self.basic_delete_functionality_test()
+
+
+class ToolTestMixin(AbstractTestMixin):
+    url = "/tools/"
+    employee_id = 0
+
+    def setUp(self):
+        Tool.objects.all().delete()
+        ToolCategory.objects.create(name="Drill", description="Drills")
+        screwdriver = ToolCategory.objects.create(
+            name="Screwdriver", description="Screwdrives"
+        )
+        Employee.objects.all().delete()
+        self.employee_id = Employee.objects.create(
+            phone_number="+380630987654", password="test"
+        ).id
+        for index in range(1, 3):
+            Tool.objects.create(
+                name=f"Screwdriver {index}-B",
+                identifier=f"S{index}",
+                category=screwdriver,
+                description="Screw it!",
+                owner_id=self.employee_id,
+            )
+
+
+class TestToolListCreateAPIView(ToolTestMixin, AbstractListCreateViewTest):
+    serializer = ToolSerializer
+    create_data = {
+        "name": "Drill SD-491 Lviv",
+        "identifier": "D-8182",
+        "category": "Drill",
+        "description": "It works. Kind of",
+    }
+
+    def get_create_data(self):
+        return {**self.create_data, "owner": self.employee_id}
+
+    def test_get_queryset(self):
+        self.get_queryset_contains_test()
+
+    def test_list_tools(self):
+        self.basic_list_functionality_test()
+
+    def test_create_tools(self):
+        self.basic_create_functionality_test()
+
+
+class TestToolRetrieveUpdateDestroyAPIView(
+    ToolTestMixin, AbstractRetrieveUpdateDestroyViewTest
+):
+    serializer = ToolSerializer
+    update_data = {
+        "name": "Drill SD-492 Kyiv",
+        "identifier": "D-8182",
+        "category": "Drill",
+        "description": "It works. Surely",
+    }
+
+    def get_update_data(self):
+        return {**self.update_data, "owner": self.employee_id}
+
+    def test_request_by_unexisting_id(self):
+        self.request_by_unexisting_id()
+
+    def test_retrieve_tool(self):
+        self.basic_retrieve_functionality_test()
+
+    def test_update_tool(self):
+        self.basic_update_functionality_test()
+
+    def test_delete_tool(self):
+        self.basic_delete_functionality_test()
