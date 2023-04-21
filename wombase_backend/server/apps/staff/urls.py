340e0480357bed3fbc44a64315a00a29f4e2237f
Andriy Smyk <105750583+smykandriy@users.noreply.github.com>
2023-04-21 12:36:04 +0300

    deleted staff app

--- wombase_backend/server/apps/staff/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import EmployeeRetrieveUpdateDestroyAPIView, EmployeeListCreateAPIView
-
-urlpatterns = [
-    path('<int:pk>', EmployeeRetrieveUpdateDestroyAPIView.as_view()),
-    path('', EmployeeListCreateAPIView.as_view()),
-]
