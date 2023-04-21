7cc9bab09cc994e7963c7acb3bd812a0c1f36408
Andriy Smyk <105750583+smykandriy@users.noreply.github.com>
2023-04-21 10:37:28 +0300

    overrided update() in details serializer

--- wombase_backend/server/apps/employee/serializers.py
+++ wombase_backend/server/apps/employee/serializers.py
@@ -10,6 +10,16 @@ class EmployeeDetailsSerializer(serializers.ModelSerializer):
         model = Employee
         fields = ('phone_number', 'first_name', 'last_name', 'password', 'email', 'role')
 
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        instance.save()
+        return instance
+
 
 class EmployeeListCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=32, write_only=True)
--- wombase_backend/server/apps/employee/views.py
+++ wombase_backend/server/apps/employee/views.py
@@ -14,16 +14,6 @@ class EmployeeRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView
     queryset = Employee.objects.all()
     serializer_class = EmployeeDetailsSerializer
 
-    def put(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        if serializer.is_valid():
-            for key, value in serializer.validated_data.items():
-                setattr(instance, key, value)
-            instance.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 class EmployeeRoleViewSet(viewsets.ModelViewSet):
     queryset = EmployeeRole.objects.all()
