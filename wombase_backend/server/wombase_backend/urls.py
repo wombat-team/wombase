db6d16e177ecc3562af875e4316b4dfa40e205f1
Andriy Smyk <105750583+smykandriy@users.noreply.github.com>
2023-05-04 02:38:54 +0300

    updated urls

--- wombase_backend/server/wombase_backend/urls.py
+++ wombase_backend/server/wombase_backend/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tools/', include('server.apps.tools.urls')),
-    path('employee/', include('server.apps.employee.urls')),
-    path('', include('server.apps.authentication.urls')),
+    path('employee/', include([
+        path('', include('server.apps.employee.urls')),
+        path('', include('server.apps.authentication.urls')),
+    ])),
 ]
