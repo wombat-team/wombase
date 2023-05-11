ac210b82ca4f3ed28eccb5c3569479b465fa9245
Andriy Smyk <105750583+smykandriy@users.noreply.github.com>
2023-05-11 22:11:23 +0300

    added permissions for employee views

--- wombase_backend/server/apps/employee/views.py
+++ wombase_backend/server/apps/employee/views.py
@@ -1,16 +1,29 @@
 from rest_framework import generics, status, viewsets
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 from rest_framework.response import Response
 
+from ..authentication.permissions import (
+    ViewRolesPermission,
+    UpdateRolesPermission,
+    UNSAFE_METHODS,
+    Permission,
+    PermissionViewMixin,
+)
 from ..core.models import Employee, EmployeeRole
-from .serializers import EmployeeDetailsSerializer, EmployeeListCreateSerializer, EmployeeRoleSerializer
+from .serializers import (
+    EmployeeDetailsSerializer,
+    EmployeeListCreateSerializer,
+    EmployeeRoleSerializer,
+    PermissionSerializer,
+)
 
 
-class EmployeeListCreateAPIView(generics.ListCreateAPIView):
+class EmployeeListCreateAPIView(PermissionViewMixin, generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeListCreateSerializer
 
 
-class EmployeeRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+class EmployeeRetrieveUpdateDestroyAPIView(PermissionViewMixin, generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeDetailsSerializer
 
@@ -21,3 +34,25 @@ class EmployeeRoleViewSet(viewsets.ModelViewSet):
 
     def update(self, request, *args, **kwargs):
         return Response({'message': 'PUT method is not available'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def get_permissions(self):
+        method = self.request.method
+        if method in SAFE_METHODS:
+            permission_classes = [ViewRolesPermission]
+        elif method in UNSAFE_METHODS:
+            permission_classes = [UpdateRolesPermission]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+
+class PermissionsListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
+    permission_classes = ()
+
+
+class PermissionsRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
+    permission_classes = ()
